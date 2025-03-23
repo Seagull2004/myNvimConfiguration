7c589cb2213c0fcb3dafa86fa466d773efbe5d56
Seagull2004 <sea2004gull@gmail.com>
2025-03-23 18:02:05 +0100

    lista configuratori di estensioni aggiunta in init.lua

--- init.lua
+++ init.lua
@@ -1,10 +1,41 @@
--- nvim stock option
+-- NVIM STOCK OPTION
 require("core.keymaps")
 require("core.autosave")
 require("core.mescalatoreLinee")
 
--- packer
+-- PACKER
 require("core.packer")
-require("core.packer_config")
 require("core.packer_config.colorscheme")
+require("core.packer_config.completions")
+require("core.packer_config.harpoon")
+require("core.packer_config.init")
+require("core.packer_config.lsp")
+require("core.packer_config.telescope")
 require("core.packer_config.tree")
+require("core.packer_config.treesitter")
+
+
+-- GENERAL EDITOR SETTINGS
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+vim.api.nvim_create_autocmd("FileType", { -- visualizzare i numeri di linea anche in netrw
+    pattern = "netrw",
+    callback = function()
+        vim.opt_local.number = true
+        vim.opt_local.relativenumber = true
+    end,
+})
+
+
+-- USE SPACES FOR TABS AND WHATNOT
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+vim.opt.softtabstop = 4
--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -18,28 +18,3 @@ vim.keymap.set('n', '<C-h>', '<C-w>h')
 -- vim.keymap.set("n", "<leader>2", ":tabmove 2")
 
 
--- GENERAL EDITOR SETTINGS
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.backspace = '2'
-vim.opt.showcmd = true
-vim.opt.laststatus = 2
-vim.opt.autowrite = true
-vim.opt.cursorline = true
-vim.opt.autoread = true
--- visualizzare i numeri di linea anche in netrw
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = "netrw",
-    callback = function()
-        vim.opt_local.number = true
-        vim.opt_local.relativenumber = true
-    end,
-})
-
-
--- USE SPACES FOR TABS AND WHATNOT
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.shiftround = true
-vim.opt.expandtab = true
-vim.opt.softtabstop = 4
