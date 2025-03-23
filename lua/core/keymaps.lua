5099ae139b03352ad067b7c39dc0b4c600544506
Seagull2004 <sea2004gull@gmail.com>
2025-03-23 17:55:04 +0100

    aggiunti alcune keymap

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,24 +1,22 @@
 -- LEADER KEY
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- Ex: cartella attuale, Rex: ultima finestra netrw aperta
+-- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- Ex vs Rex
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
--- vim.keymap.set("i", "jk", "<Esc>") -- premere jk per tornare in normal mode
 -- vim.keymap.set("n", "<C-w>", "<C-w>v") -- come su obsidian
 vim.keymap.set('v', '<Leader>cp', '"+y')
 vim.keymap.set('n', '<Leader>cpa', 'ggVG"+y<C-o>')
--- vim.api.nvim_set_keymap('v', '<Leader>cp', '"+y', { noremap = true, silent = true })
---vim.api.nvim_set_keymap('n', '<Leader>cpa', 'ggVG"+y', { noremap = true, silent = true })
+vim.keymap.set('n', '<Leader>dd', 'ggdG')
 
--- gestione del multi window
+
+-- MULTI WINDOW
 vim.keymap.set('n', '<Leader>n', '<C-w>v')
 vim.keymap.set('n', '<C-l>', '<C-w>l') -- per farlo funzionare è stata modificata la riga 6442 del file /opt/homebrew/Cellar/neovim/0.10.2_1/share/nvim/runtime/autoload/netrw.vim
 vim.keymap.set('n', '<C-h>', '<C-w>h')
-
-
 -- vim.keymap.set("n", "<leader>a", vim.cmd.tabnew)
 -- vim.keymap.set("n", "<leader>1", ":tabmove 1")
 -- vim.keymap.set("n", "<leader>2", ":tabmove 2")
+
 
 -- GENERAL EDITOR SETTINGS
 vim.opt.number = true
@@ -37,6 +35,7 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.opt_local.relativenumber = true
     end,
 })
+
 
 -- USE SPACES FOR TABS AND WHATNOT
 vim.opt.tabstop = 4
