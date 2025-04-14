16cfef43a97f8187f8afb706c86e2835da1fbf82
Seagull2004 <sea2004gull@gmail.com>
2025-04-14 11:15:55 +0200

    migliorati alcune keymap

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -3,10 +3,9 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 -- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- Ex vs Rex
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
--- vim.keymap.set("n", "<C-w>", "<C-w>v") -- come su obsidian
 vim.keymap.set('v', '<Leader>cp', '"+y')
-vim.keymap.set('n', '<Leader>cpa', 'ggVG"+y<C-o>')
-vim.keymap.set('n', '<Leader>dd', 'ggdG')
+vim.keymap.set('n', '<Leader>ca', 'gg"+yG<C-o>')
+vim.keymap.set('n', '<Leader>da', 'ggdG')
 
 -- gestire lo scrolling del file in modo che il cursore rimanga centrato
 vim.keymap.set('n', '<C-d>', function()
@@ -35,8 +34,6 @@ end, { silent = true })
 
 -- MULTI WINDOW
 vim.keymap.set('n', '<Leader>n', '<C-w>v')
-vim.keymap.set('n', '<C-l>', '<C-w>l') -- per farlo funzionare è stata modificata la riga 6442 del file /opt/homebrew/Cellar/neovim/0.10.2_1/share/nvim/runtime/autoload/netrw.vim
 vim.keymap.set('n', '<C-h>', '<C-w>h')
--- vim.keymap.set("n", "<leader>a", vim.cmd.tabnew)
--- vim.keymap.set("n", "<leader>1", ":tabmove 1")
--- vim.keymap.set("n", "<leader>2", ":tabmove 2")
+vim.keymap.set('n', '<C-l>', '<C-w>l') -- per farlo funzionare è stata modificata la riga 6442 del file /opt/homebrew/Cellar/neovim/0.10.2_1/share/nvim/runtime/autoload/netrw.vim
+-- vim.keymap.set("n", "<C-w>", "<C-w>v") -- come su obsidian
