bc0ae8e749f4a1a1a1dd2b087400f81aea9d3f5b
Seagull2004 <sea2004gull@gmail.com>
2025-04-24 19:08:49 +0200

    migliorato ux fuzzy finder

--- lua/core/packer_config/telescope.lua
+++ lua/core/packer_config/telescope.lua
@@ -1,8 +1,14 @@
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
--- vim.keymap.set('n', '<C-p>', builtin.git-files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Search for somethin' > ") });
-end)
+vim.keymap.set('n', '<leader>pl', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
+
+--
+--vim.keymap.set('n', '<C-p>', builtin.git-files, {})
+-- vim.keymap.set('n', '<leader>ps', function()
+-- 	builtin.grep_string({ search = vim.fn.input("Search for somethin' > ") });
+-- end)
+
+
 
